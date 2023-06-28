4e8428df187eab49125ca0cd8a94909ea38648af
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2023-06-28 10:16:48 +0200

    deps

--- apps/client/src/routes/+layout.svelte
+++ apps/client/src/routes/+layout.svelte
@@ -1,11 +1,7 @@
-<script lang="ts">
-	import '../app.css'
-</script>
-
 <svelte:head>
 	<title>Oxygen - API Devtool</title>
 	<meta name="description" content="Oxygen - API Devtool In Svelte" />
 </svelte:head>
 
-<span class="bg-red-500 text-slate-100">layout</span>
+<span class="text-xl font-black">layout</span>
 <slot />
